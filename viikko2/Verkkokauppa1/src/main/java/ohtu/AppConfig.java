1b01d8ea40d483d7fc4abe57acf553f2f62429e6
Erkkapyy <erkka.pyykonen@gmail.com>
2019-11-05 18:18:23 +0200

    Finish week 2 exercises

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/AppConfig.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author pyykonee
+ */
+@Configuration
+@ComponentScan(basePackages = "ohtu.verkkokauppa")
+public class AppConfig  {}
